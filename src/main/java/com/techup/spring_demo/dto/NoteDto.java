ac435b5070533a72d0e9aea5ac2f3d120525bf1a
Bambam <punyanuch.ksd@gmail.com>
2025-11-10 17:13:20 +0700

    Create DTOs for API communication
    - Add RegisterRequest & LoginRequest for auth endpoints
    - Add AuthResponse for authentication responses
    - Add NoteDto for note data transfer
    - Setup request/response data structures

---
+++ src/main/java/com/techup/spring_demo/dto/NoteDto.java
@@ -0,0 +1,16 @@
+package com.techup.spring_demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NoteDto {
+    private Long id;
+    private String title;
+    private String content;
+}
---
+++ src/main/java/com/techup/spring_demo/dto/NoteRequest.java
@@ -0,0 +1,5 @@
+package com.techup.spring_demo.dto;
+
+public class NoteRequest {
+    
+}
